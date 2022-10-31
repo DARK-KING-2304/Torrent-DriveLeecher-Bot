778009c5efea3d1d95cab83cd2c74948baba9471
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2022-10-31 21:09:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // const tgToken = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"; //aryantestbot
 const tgToken = "5693964146:AAHyEqbFGNqyGFfszv1t1yEUObqXdngni2k"; //gtorrent
 const downloadFolder = "downloads";
-const mongoUrl = "mongodb+srv://aryanvikash:vikash12@cluster0-mp4rw.mongodb.net/test?retryWrites=true&w=majority"
+const mongoUrl = "mongodb+srv://ub:ub123@horivc.cemtd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 // const maxSize = 10;
 
 module.exports = { tgToken, downloadFolder, mongoUrl };
